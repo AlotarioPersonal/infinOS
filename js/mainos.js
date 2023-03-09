5bf29005880eabcd83427ba8194571a709043a7d
Alot <126506217+AlotarioPersonal@users.noreply.github.com>
2023-03-09 10:15:10 -0600

    added cursor functionality?

--- js/mainos.js
+++ js/mainos.js
@@ -5,7 +5,100 @@ var ctx = canvas.getContext("2d");
 taskbarX = 0
 taskbarY = canvas.height - 45
 
-// DRAW
+cursorX = 100
+cursorY = 100
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+function keyDownHandler(e) {
+    if ("code" in e) {
+        switch(e.code) {
+            case "Unidentified":
+                break;
+            case "ArrowRight":
+            case "Right": // IE <= 9 and FF <= 36
+            case "KeyD":
+                rightPressed = true;
+                return;
+            case "ArrowLeft":
+            case "Left": // IE <= 9 and FF <= 36
+            case "KeyA":
+                leftPressed = true;
+                return;
+            case "ArrowUp":
+            case "Up": // IE <= 9 and FF <= 36
+            case "KeyW":
+                upPressed = true;
+                return;
+            case "ArrowDown":
+            case "Down": // IE <= 9 and FF <= 36
+            case "KeyS":
+                downPressed = true;
+                return;
+            default:
+                return;
+        }
+    }
+
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+    if(e.keyCode == 40) {
+        downPressed = true;
+    }
+    else if(e.keyCode == 38) {
+        upPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if ("code" in e) {
+        switch(e.code) {
+            case "Unidentified":
+                break;
+            case "ArrowRight":
+            case "Right": // IE <= 9 and FF <= 36
+            case "KeyD":
+                rightPressed = false;
+                return;
+            case "ArrowLeft":
+            case "Left": // IE <= 9 and FF <= 36
+            case "KeyA":
+                leftPressed = false;
+                return;
+            case "ArrowUp":
+            case "Up": // IE <= 9 and FF <= 36
+            case "KeyW":
+                upPressed = false;
+                return;
+            case "ArrowDown":
+            case "Down": // IE <= 9 and FF <= 36
+            case "KeyS":
+                downPressed = false;
+                return;
+            default:
+                return;
+        }
+    }
+
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+    if(e.keyCode == 40) {
+        downPressed = false;
+    }
+    else if(e.keyCode == 38) {
+        upPressed = false;
+    }
+}
+
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -13,6 +106,22 @@ function draw() {
     ctx.fillRect(taskbarX, taskbarY, canvas.width, 45);
     ctx.fillStyle = "#000000";
     ctx.fillRect(taskbarX + 5, taskbarY + 5, 32, 32)
+    ctx.fillStyle = "#FFFFFF"
+    ctx.fillRect(cursorX, cursorY, 16, 16)
+
+    // KEYBOARD
+    if(rightPressed) {
+        cursorX += 3;
+    }
+    else if(leftPressed) {
+        cursorX -= 3;
+    }
+    if(downPressed) {
+        cursorY += 3;
+    }
+    else if(upPressed) {
+        cursorY -= 3;
+    }
     
 
     requestAnimationFrame(draw)
